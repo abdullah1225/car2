132f61d2a8fa38b3763f37759da63cfcdf6991e0
abdullah1225 <abdullah_1225@yahoo.com>
2016-12-26 21:38:39 +0300

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,13 +12,25 @@ function ($scope, $stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $http) {
+$scope.connected=false;
+
+
+
+
+
+
+
 	
 $scope.push=function(x){
 	$http.defaults.headers.post["Content-Type"] = "application/json";
 	console.log(x);
-    $http.post('http://192.168.43.159:5000/foo',{hi:x})
+    $http.post('http://192.168.137.15:5000/foo',{hi:x})
 		.success(function(resp){
 			console.log(resp);
+			if(Object.keys(resp).length>=1){
+				$scope.connected=true;
+			};
+			console.log(Object.keys(resp).length);
 		})
 	
 };
